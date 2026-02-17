84513adc54a0efb001208d65b894c865f2e66395
qa-user1 <protractor.user1@gmail.com>
2026-02-17 18:14:04 +0100

    .

--- cypress.config.js
+++ cypress.config.js
@@ -20,7 +20,7 @@ module.exports = defineConfig({
     retries: {
         // Configure retry attempts for `cypress run`
         // Default is 0
-        runMode: 0,
+        runMode: 4,
         // Configure retry attempts for `cypress open`
         // Default is 0
         openMode: 0
